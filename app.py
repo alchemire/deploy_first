b7aa3dd34fb7f02dea6f4f24b199e84fbe9c20a7
alchemire <wannataraxia@gmail.com>
2023-11-05 13:02:51 +0900

    add device(cpu)

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import json
 
 st.title("温故知新 feat.青空文庫")
 
-input_text = st.text_input("つれづれなるままに、日暮らし硯に向かひて、心にうつりゆくよしなしごとを、そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。")
-if st.button("現代語訳"):
-    st.text("手持ちぶさたなのにまかせて、一日中硯に向かって、心に浮かんだり消えたりしてうつっていくつまらないことを、とりとめもなく書きつけると、妙に正気を失った気分になる。")
+input_text = st.text_input("青空文庫に掲載されている、1000文字以下の作品2995点から、コサイン類似度が高い文章TOP5を返します。先人たちに聞き役になってもらい、一緒に内省を深めましょう。")
+if st.button("徒然草"):
+    st.text("つれづれなるままに、日暮らし硯に向かひて、心にうつりゆくよしなしごとを、そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。")
+if st.button("徒然草(現代語訳)"):
+    st.text("することもなく手持ちぶさたなのにまかせて、一日中、硯に向かって、心の中に浮かんでは消えていくたわいもないことを、とりとめもなく書きつけていると、思わず熱中して不思議と、気が変になる。")
 
 data = {
     "input_text" : input_text
@@ -20,4 +22,6 @@ if st.button("click"):
 
     response = requests.post("http://localhost:8000/embedding", data=data_json)
     response_data = response.json()
-    st.write(response_data)
+    st.subheader("類似度の高い文章TOP5")
+    with st.expander("1"):
+        st.write(response_data)
--- main.py
+++ main.py
@@ -15,14 +15,11 @@ app = FastAPI()
 class text(BaseModel):
     input_text: str
 
-@app.get("/")
-def index():
-    return{"温故知新": "類似度の高い青空文庫の作品を返す"}
 
 # enbeddingしたリストを読み込み。
 load_embeddings = []
-# with open(r"C:\Users\wanna\OneDrive\デスクトップ\deploy_first\embedding_list.pkl", "rb") as file:
-with open("embedding_list.pkl", "rb") as file:
+with open(r"C:\Users\wanna\OneDrive\デスクトップ\deploy_first\embedding_list.pkl", "rb") as file:
+# with open("embedding_list.pkl", "rb") as file:
   loaded_tensor_list = pickle.load(file)
   for tensor in loaded_tensor_list:
      load_embeddings.append(tensor)
@@ -30,19 +27,25 @@ with open("embedding_list.pkl", "rb") as file:
 # テキストと作者の読み込み
 text_list=[]
 author_list=[]
-# with open(r"C:\Users\wanna\OneDrive\デスクトップ\deploy_first\under_1000.csv", mode="r", encoding="utf-8") as file:
-with open("under_1000.csv", mode="r", encoding="utf-8") as file:
+with open(r"C:\Users\wanna\OneDrive\デスクトップ\deploy_first\under_1000.csv", mode="r", encoding="utf-8") as file:
+# with open("under_1000.csv", mode="r", encoding="utf-8") as file:
    csv_reader = csv.reader(file)
    next(csv_reader)
    for row in csv_reader:
       text_list.append(row[1])
       author_list.append(row[0])
 
+device = torch.device('cpu')
+
 # モデルの読み込み
 model_name='cl-tohoku/bert-base-japanese-whole-word-masking'
 bert_tokenizer = BertJapaneseTokenizer.from_pretrained(model_name)
-bert_model = BertModel.from_pretrained(model_name)
+bert_model = BertModel.from_pretrained(model_name).to(device)
 cos =torch.nn.CosineSimilarity(dim=0)
+
+@app.get("/")
+def index():
+    return{"温故知新": "類似度の高い青空文庫の作品を返す"}
 
 # embedding input_text, then sort by similarity 
 @app.post("/embedding")
@@ -55,7 +58,7 @@ def get_bert_embeddings_similar_sort(input_text: text):
         truncation=True,
         padding="max_length",
         max_length=512
-    )
+    ).to(device)
     attention_mask=[int(token_id) > 0 for token_id in tokenized_sent[0]]
     
     with torch.no_grad():
